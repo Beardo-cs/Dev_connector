2bbbfe320a415dcbb58a38875316e75a87cd1270
Mahendiran <mahemahe124@gmail.com>
2022-04-11 16:11:59 +0530

    added api running

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+//api
+app.get('/', (req, res) => res.send('API Running'));
+
+
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
